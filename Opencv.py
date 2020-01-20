0c81505e2e19ccbe4d43cf15233389f01828b5d5
Pranshul2002 <57452974+Pranshul2002@users.noreply.github.com>
2020-01-20 18:58:43 +0530

    Update Opencv.py

--- Opencv.py
+++ Opencv.py
@@ -18,29 +18,24 @@ cv2.createTrackbar("uh", "HSV",179, 179, nothing);
 cv2.createTrackbar("hs", "HSV",255, 255, nothing);
 cv2.createTrackbar("hv", "HSV",255, 255, nothing);
 cv2.createTrackbar("save","HSV",0,1,save);
-cv2.namedWindow("circle")
-cv2.createTrackbar("a1","circle",0,200,nothing)
-cv2.createTrackbar("a2","circle",0,200,nothing)
-cv2.createTrackbar("e1","circle",0,200,nothing)
-cv2.createTrackbar("e2","circle",0,200,nothing)
-cv2.createTrackbar("minrad","circle",0,200,nothing)
-cv2.setTrackbarPos('minrad',"circle",10)
-cv2.setTrackbarPos('a1',"circle",85)
-cv2.setTrackbarPos('a2',"circle",125)
-cv2.setTrackbarPos('e1',"circle",85)
-cv2.setTrackbarPos('e2',"circle",125)
 
-cv2.setTrackbarPos('lh',"HSV",25)
-cv2.setTrackbarPos('ls',"HSV",80)
-cv2.setTrackbarPos('lv',"HSV",58)
-cv2.setTrackbarPos('uh',"HSV",47)
-cv2.setTrackbarPos('hs',"HSV",219)
+minrad = 15
+a1 = 0.85
+a2 = 1.25
+e1 = 0.85
+e2 = 1.25
+cv2.setTrackbarPos('lh',"HSV",30)
+cv2.setTrackbarPos('ls',"HSV",60)
+cv2.setTrackbarPos('lv',"HSV",40)
+cv2.setTrackbarPos('uh',"HSV",111)
+cv2.setTrackbarPos('hs',"HSV",255)
 cv2.setTrackbarPos('hv',"HSV",255)
 cap = cv2.VideoCapture(0)
+po = 0
 while(True):
     z,frame = cap.read()
     if(z==True):
-        HSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        HSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HLS_FULL)
         g = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         lh = cv2.getTrackbarPos('lh', "HSV")
         ls = cv2.getTrackbarPos('ls', "HSV")
@@ -48,33 +43,27 @@ while(True):
         uh = cv2.getTrackbarPos('uh', "HSV")
         hs = cv2.getTrackbarPos('hs', "HSV")
         hv = cv2.getTrackbarPos('hv', "HSV")
-        a1 = cv2.getTrackbarPos('a1',"circle")
-        a2 = cv2.getTrackbarPos('a2',"circle")
-        e1 = cv2.getTrackbarPos('e1', "circle")
-        e2 = cv2.getTrackbarPos('e2', "circle")
-        minrad = cv2.getTrackbarPos('minrad',"circle")
-        a1 = a1/100
-        a2 = a2 / 100
-        e1 = e1 / 100
-        e2 = e2 / 100
         thresh1 = np.array([lh,ls,lv])
         thresh2 = np.array([uh,hs,hv])
+
+
         mask = cv2.inRange(HSV,thresh1,thresh2)
 
         kernel = np.ones((2,2),np.uint8)/4
         mask  = cv2.erode(mask,kernel,iterations=2)
-        mask = cv2.dilate(mask,kernel,iterations=3)
-        #mask = cv2.medianBlur(mask,5)
+        mask = cv2.dilate(mask,kernel,iterations=1)
 
+        #mask = cv2.medianBlur(mask,5)
+        k =1
         image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_L1)
-        HSV1 = cv2.bitwise_and(frame, frame, mask=mask)
+        HSV1 = cv2.bitwise_and(frame,frame,mask=mask)
 
         for i in range(0,len(contours)):
-            if(len(contours)>1):
+            if len(contours)>1:
                 (x,y),radius = cv2.minEnclosingCircle(contours[i])
                 center = (int(x),int(y))
                 radius = int(radius)
-                x, y, w, h = cv2.boundingRect(contours[i])
+                x1, y1, w, h = cv2.boundingRect(contours[i])
                 aspect_ratio = float(w) / h
                 area1 = np.pi * radius * radius
                 area2 = cv2.contourArea(contours[i])
@@ -84,11 +73,15 @@ while(True):
                     e = 0
 
             if(aspect_ratio > a1 and aspect_ratio < a2 and e>e1 and e < e2 and minrad<radius):
-                HSV1 = cv2.circle(HSV1,center,radius,(0,0,255),2)
-                print("BALL!!!!")
+                frame = cv2.circle(frame,center,radius,(0,0,255),2)
+                frame = cv2.putText(frame,"Ball",(int(x-radius),int(y-radius)),fontFace=cv2.FONT_HERSHEY_COMPLEX,fontScale=1,color=(0,0,255))
+                print("BALL!!!!",po)
+                po = po + 1
+
         else:
             pass
-        cv2.imshow("HSV",HSV1)
+
+        cv2.imshow("HSV",frame)
 
         if(cv2.waitKey(1) & 0xFF == 27):
             break
