f94843e70d70eed1530f1edaad15db9aecb8cc04
Pranshul2002 <pranshul20162002@gmail.com>
2020-02-02 19:28:38 +0530

    commit

--- Twocamera.py
+++ Twocamera.py
@@ -1,15 +1,44 @@
 import numpy as np
 import cv2
-cap = cv2.VideoCapture(0)
-cap1 = cv2.VideoCapture(2)
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    ret1, frame1 = cap1.read()
-    if ret==True and ret1==True:
-        frame = np.concatenate((frame,frame1),axis=1)
-        cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-cap.release()
-cap1.release()
-cv2.destroyAllWindows()
+import threading
+import time
+global flag 
+flag = 0
+def Cap():
+    cap = cv2.VideoCapture(0)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if(ret):
+            cv2.imshow("frame",frame)
+        if(cv2.waitKey(1) & 0xFF == 27):
+            break
+        if(flag == 1):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+def Cap1():
+    cap1 = cv2.VideoCapture(2)
+    while(cap1.isOpened()):
+        ret1, frame1 = cap1.read()
+        if(ret1):
+            cv2.imshow("frame1",frame1)
+        if(cv2.waitKey(1) & 0xFF == 27):
+            break
+        if(flag == 1):
+            break
+    cap1.release()
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    e1 = threading.Event()
+    t = threading.Thread(target=Cap)
+    t1 = threading.Thread(target=Cap1)
+    t.start()
+    t1.start()
+try:
+    while 1:
+        time.sleep(.1)
+except KeyboardInterrupt:
+    flag = 1
+    t.join()
+    t1.join()
+
--- client.py
+++ client.py
@@ -26,7 +26,48 @@ while 1:
 
         data_arr = cv2.imdecode(data_arr,cv2.IMREAD_ANYCOLOR)
         data_arr = cv2.resize(data_arr,(640,480))
-        cv2.imshow('frame',data_arr)
+        HSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HLS_FULL)
+        thresh1 = np.array([lh,ls,lv])
+        thresh2 = np.array([uh,hs,hv])
+        mask = cv2.inRange(HSV,thresh1,thresh2)
+        mask = cv2.GaussianBlur(mask,(7,7),0)
+        image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_L1)
+        HSV1 = cv2.bitwise_and(frame,frame,mask=mask)
+
+        for i in range(0,len(contours)):
+            if len(contours)>1:
+                (x,y),radius = cv2.minEnclosingCircle(contours[i])
+                center = (int(x),int(y))
+                radius = int(radius)
+                x1, y1, w, h = cv2.boundingRect(contours[i])
+                aspect_ratio = float(w) / h
+                area1 = np.pi * radius * radius
+                area2 = cv2.contourArea(contours[i])
+                if(area1 != 0 and area2 != 0 ):
+                    e = area1/area2
+                else:
+                    e = 0
+                area = cv2.contourArea(contours[i])
+                hull = cv2.convexHull(contours[i])
+                hull_area = cv2.contourArea(hull)
+                if(hull_area != 0):
+                    solidity = float(area) / hull_area
+                else:
+                    solidity = 0
+            if(aspect_ratio > a1 and aspect_ratio < a2 and e>e1 and e < e2 and minrad<radius and solidity < 1 and solidity > a1 ):
+                frame = cv2.drawContours(frame,contours,i,color=(255,0,0),thickness=3)
+                frame = cv2.circle(frame,center,radius,(0,0,255),2)
+                frame = cv2.putText(frame,"Ball",(int(x-radius),int(y-radius)),fontFace=cv2.FONT_HERSHEY_COMPLEX,fontScale=1,color=(0,0,255))
+                frame = cv2.putText(frame, "Ball detected", (380,30),
+                                    fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(0, 0, 255),thickness=3)
+
+        else:
+            pass
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        cv2.imshow("HSV",frame)
+        cv2.imshow("mask",mask)
+        if(cv2.waitKey(1) & 0xFF == 27):
+            break
     except:
         pass
     if(cv2.waitKey(1) & 0xFF == 27):
